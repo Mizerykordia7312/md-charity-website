eb531bcecbc77b527a689dafc23da26066a1f3e5
Mateusz Szymański <Mizerykordia12@interia.pl>
2025-04-18 15:30:24 +0200

    add hero section

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,5 +1,18 @@
+import classes from './MainPage.module.scss';
+import { Link } from 'react-router-dom';
+
 const MainPage = () => {
-	return <h1>Main Page</h1>;
+	return (
+		<section className={classes.heroSection}>
+			<div className={classes.heroContent}>
+				<h1>Fundacja Maskotki dzieciom</h1>
+				<p>Pomagamy dzieciom z autyzmem w całym kraju</p>
+				<Link to='help-us' className={classes.button}>
+					Wesprzyj
+				</Link>
+			</div>
+		</section>
+	);
 };
 
 export default MainPage;
